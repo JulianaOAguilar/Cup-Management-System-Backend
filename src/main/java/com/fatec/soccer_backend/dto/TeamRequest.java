3271afce4a48ed4deb2cd051b60236dd16e8e42a
Julianaoaguilar <julianaoaguilar16@gmail.com>
2026-06-08 10:45:07 -0300

    Validators

--- src/main/java/com/fatec/soccer_backend/dto/TeamRequest.java
+++ src/main/java/com/fatec/soccer_backend/dto/TeamRequest.java
@@ -1,11 +1,26 @@
 package com.fatec.soccer_backend.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 
 public record TeamRequest(
 
+    @NotBlank
     String country,
+
+    @NotBlank
+    @Size(min=3, max=3)
     String fifaCode,
+
+    @NotBlank
     String coach,
+
+    @NotBlank
+    @Min(value = 3)
+    @Max(value=32)
     Integer playerQuantity
 
 ) {
