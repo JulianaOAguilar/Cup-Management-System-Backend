ac6c07a42cda39d6837c38afd5b70e689c713802
Julianaoaguilar <julianaoaguilar16@gmail.com>
2026-06-07 22:25:23 -0300

    Structure

---
+++ src/main/java/com/fatec/soccer_backend/SoccerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fatec.soccer_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoccerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoccerBackendApplication.class, args);
+	}
+
+}
