c82a60be8c8a5a83a433f1de977a2b7802539d4d
Julianaoaguilar <julianaoaguilar16@gmail.com>
2026-06-14 22:23:00 -0300

    Validations to tournamentRequest

--- src/main/java/com/fatec/soccer_backend/dto/TournamentRequest.java
+++ src/main/java/com/fatec/soccer_backend/dto/TournamentRequest.java
@@ -4,12 +4,24 @@ import java.time.LocalDateTime;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record TournamentRequest(
 
+        @NotNull(message = "Team 1 é obrigatório")
         Long team1Id,
+
+        @NotNull(message = "Team 2 é obrigatório")
         Long team2Id,
+
+        @NotBlank(message = "Local do jogo é obrigatório")
         String location,
 
+        @NotNull(message = "Data do jogo é obrigatória")
+        @Future(message = "A data do jogo deve ser no futuro")
         @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
         LocalDateTime matchDateTime
 ) {}
+
